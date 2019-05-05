c4106e5529ab09b0830c94e316865202cce97e40
BarracudaPff <mrm.kikll@gmail.com>
2019-05-05 22:03:51 +0300

    fx naming

--- sql/20190428225137_create_payments_table.sql
+++ sql/20190428225137_create_payments_table.sql
@@ -7,7 +7,7 @@ CREATE TABLE "payments" (
     "body"                BIGINT    NOT NULL,
     "type"                TEXT      NOT NULL,
     "remain_credit_body"  BIGINT    NOT NULL,
-    "fullEarly_repayment" BIGINT    NOT NULL,
+    "full_early_repayment" BIGINT    NOT NULL,
     CONSTRAINT payments_type_check CHECK ("type" IN ('regular', 'early')),
     PRIMARY KEY (id)
 );
